0958bbe06acb503d20c4d03100c475f28ea1ca3a
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-15 18:32:56 +0800

    n

--- string.cc
+++ string.cc
@@ -2,8 +2,10 @@
 #include <string>
 using std::string;
 int main() {
-  string a="hello world";
-  const char* ch=a.c_str();
-  printf("%s \n",ch);
+  string strSrc="hello world";
+  const char* chSrc=strSrc.c_str();
+  printf("%s \n",chSrc);
+  string str_src=chSrc;
+  printf("%s \n",str_src.c_str());
   return 0;
 }
