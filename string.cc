affacce80b468b0db161c3952dbc3b3f4bc4dd54
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-08 15:00:55 +0800

    n

--- string.cc
+++ string.cc
@@ -13,5 +13,24 @@ int main() {
   string.ShowString();
   string.WordRev();
   string.ShowString();
+
+  char * p="abc";
+  //p[0]='a'; //run-time error
+  cout<<sizeof(p)/sizeof(char)<<endl;
+
+  p=new char[4];
+  p[0]='a';
+  p[1]='a';
+  p[2]='a';
+  p[3]='a';
+  p[4]='a';
+  p[5]='a';
+  cout<<sizeof(p)/sizeof(char)<<endl;
+  cout<<p<<endl;
+
+  char p1[]="abcdefgh";
+  p1[1]='a';
+  cout<<sizeof(p1)/sizeof(char)<<endl;
+  cout<<p1<<endl;
   return 0;
 }
