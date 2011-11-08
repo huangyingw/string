4f14bf07eed3b366f01460dd0b9a0a00676a3db1
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-08 14:38:20 +0800

    n

--- string.cc
+++ string.cc
@@ -1,11 +1,12 @@
+#include "string.h"
 #include <stdio.h>
-#include <string>
-using std::string;
-int main() {
-  string strSrc="hello world";
-  const char* chSrc=strSrc.c_str();
-  printf("%s \n",chSrc);
-  string str_src=chSrc;
-  printf("%s \n",str_src.c_str());
+int main(void)
+{
+	String string("A BC DEF");
+	string.ShowString();
+	string.StringCat(" GHIJ");
+	string.ShowString();
+	string.WordRev();
+	string.ShowString();
   return 0;
 }
---
+++ string.h
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+using namespace std;
+
+class String
+{
+public:
+	String(const char* str);
+	~String();
+	int GetLength(const char* str);
+	char* StringCat(const char* str);
+	void ShowString();
+	void StringCat(char* str);
+	void WordRev();
+	void StringRev();
+private:
+	ofstream fout;
+	char *data;
+	char* newStr;
+};
+
+String::String(const char* str)
+{
+	data=new char[GetLength(str)];
+	char *temp=data;
+	while(*temp++=*str++);
+	newStr=NULL;
+	fout.open("output.txt");
+}
+
+String::~String()
+{
+	if(NULL!=data)
+	{
+		delete[] data;
+	}
+	
+	if(NULL!=newStr)
+	{
+		delete[] newStr;
+	}
+	fout.close();
+}
+
+void String::StringRev()
+{
+	/*
+	char *nav, *p;
+	char space[]=" ";
+	newStr=new char [GetLength(data)];
+	nav=data;
+	while(*nav!='\0')
+		nav++;
+	while(*nav!=' ')
+		--nav;
+	strcat(newStr, nav);
+	strcat(newStr, space);
+	fout<<"newStr0 string->"<<newStr<<endl<<endl;
+	
+	while(nav>data)
+	{
+		fout<<"nav0->"<<*nav<<endl;
+		while(*nav==' ')
+			--nav;
+
+		fout<<"nav1->"<<*nav<<endl;
+		
+		*++nav='\0';
+		
+		while(*nav!=' ' && nav>data)
+			nav--;
+		
+		fout<<"nav2->"<<*nav<<endl;
+		
+		fout<<"nav string->"<<nav<<endl;
+		fout<<"newStr1 string->"<<newStr<<endl;
+		
+		
+		strcat(newStr, nav);
+		strcat(newStr, space);
+		fout<<"result->"<<newStr<<endl;
+		fout<<endl;
+		
+	}	
+	*/
+}
+
+void String::WordRev()
+{
+	char *nav, *p;
+	char space[]=" ";
+	newStr=new char [GetLength(data)];
+	nav=data;
+	while(*nav!='\0')
+		nav++;
+	while(*nav!=' ')
+		--nav;
+	strcat(newStr, nav);
+	strcat(newStr, space);
+	
+	while(nav>data)
+	{
+		while(*nav==' ')
+			--nav;
+		*++nav='\0';
+		while(*nav!=' ' && nav>data)
+			nav--;
+		strcat(newStr, nav);
+		strcat(newStr, space);
+	}	
+	data=newStr;
+}
+
+char* String::StringCat(const char* str)
+{
+	char* target = (char*) realloc (NULL,(GetLength(data)+GetLength(str)) * sizeof(char));
+	char * source=data;
+	char * p=target;
+	while(*source)
+	{
+		*p++=*source++;
+	}
+	while(*str)
+	{
+		*p++=*str++;
+	}
+	*++p='\0';
+	data=target;
+}
+
+void String::ShowString()
+{
+	fout<<data<<endl;
+}
+
+int String::GetLength(const char* str)
+{
+	int len=0;
+	while(*str++!='\0')
+	{
+		len++;
+	}
+	return len;
+}
